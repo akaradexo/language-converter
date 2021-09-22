9b5e2afe14f06ed91efe8ff3ca6a48d8788377b7
Ashutosh Karade <69238714+xR0v3Rx@users.noreply.github.com>
2021-09-22 10:34:27 +0530

    ADDED : gif | select option | responsive

--- script/script.js
+++ script/script.js
@@ -1,1 +1,39 @@
-"use strict";
+'use strict';
+const input = document.getElementById('input');
+
+const btnTranslate = document.getElementById('btn-translate');
+
+const output = document.getElementById('output');
+
+const language =  document.getElementById('language');
+
+const translateText = async function(){
+  const inputText = input.value;
+
+if(inputText === '' || inputText === null){
+  alert('Input Text is empty. What are you going to translate? blank space?')
+}else{
+  const languageFun = language.value;
+
+  let url = `https://api.funtranslations.com/translate/${languageFun}.json?text=${inputText}`;
+  console.log(url);
+  const data = await fetch(url);
+  console.log(data);
+
+  const result = await data.json();
+  console.log(result);
+
+  output.innerHTML = result.contents.translated;
+
+}
+}
+
+const checkKey = function(e) {
+  if(e.key === 'Enter'){
+    translateText();
+  }
+}
+
+btnTranslate.addEventListener('click',translateText);
+
+document.addEventListener('keydown',checkKey)
